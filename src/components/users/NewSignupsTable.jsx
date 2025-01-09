d281f5b52d563977c748e20a6dfdc6b0f301b1fa
Andrew Kicklighter <apkicklighter@gmail.com>
2025-01-09 02:29:42 -0600

    fix not building issue

--- src/components/users/NewSignupsTable.jsx
+++ src/components/users/NewSignupsTable.jsx
@@ -24,6 +24,7 @@ const NewSignupsTable = ({
                         handleChangePage,
                         handleChangeRowsPerPage
                     }) => {
+    const [selectedRole] = useState('');
     const navigate = useNavigate();
 
     const handleRowClick = (username) => {
