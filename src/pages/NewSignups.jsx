3a7e41ac73ba95b1518e180ed5a2d3b9245146ab
Andrew Kicklighter <apkicklighter@gmail.com>
2025-02-10 21:41:55 -0600

    add open teams to new signups

--- src/pages/NewSignups.jsx
+++ src/pages/NewSignups.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { CircularProgress, Box } from '@mui/material';
+import {CircularProgress, Box, Typography, Card, useTheme} from '@mui/material';
 import NewSignupsTable from '../components/users/NewSignupsTable'; // Import the table component
 import {useNavigate} from "react-router-dom";
 import ErrorMessage from "../components/message/ErrorMessage";
-import {getNewSignups} from "../api/newSignupsApi"; // Assuming you have userApi for fetching users
+import {getNewSignups} from "../api/newSignupsApi";
+import {Header} from "../styles/GamesStyles";
+import LoadingSpinner from "../components/icons/LoadingSpinner";
+import TeamsTable from "../components/team/TeamsTable";
+import {getOpenTeams, getTeamByName} from "../api/teamApi";
 
 const NewSignupsPage = ({ user }) => {
+    const theme = useTheme();
     const navigate = useNavigate();
+    const [teams, setTeams] = useState([]);
     const [users, setNewSignups] = useState([]);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -43,8 +49,27 @@ const NewSignupsPage = ({ user }) => {
             }
         };
 
+        const fetchTeams = async () => {
+            try {
+                const data = await getOpenTeams();
+                const teams = await Promise.all(data.map(team => getTeamByName(team)));
+                setTeams(teams);
+                setLoading(false);
+            } catch (error) {
+                setError('Failed to load teams');
+                setLoading(false);
+            }
+        };
+
+        fetchTeams();
         fetchNewSignups();
     }, []);
+
+    const sortedTeams = teams.sort((a, b) => {
+        if (a[orderBy] < b[orderBy]) return order === 'asc' ? -1 : 1;
+        if (a[orderBy] > b[orderBy]) return order === 'asc' ? 1 : -1;
+        return 0;
+    });
 
     const handleRequestSort = (property) => {
         const isAsc = orderBy === property && order === 'asc';
@@ -61,6 +86,29 @@ const NewSignupsPage = ({ user }) => {
         setPage(0);
     };
 
+    const handleTeamRequestSort = (property) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    const handleTeamChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleTeamChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const handleNullValue = (value, fallback = '-') => {
+        return value ? value : fallback;
+    };
+
+    const handleArrayValue = (array, fallback = '-') => {
+        return array && array.length > 0 ? array.join(', ') : fallback;
+    };
+
     if (loading) {
         return (
             <Box sx={{ py: 2, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -74,19 +122,50 @@ const NewSignupsPage = ({ user }) => {
     }
 
     return (
-        <div>
-            <h2>New Signups</h2>
-            <NewSignupsTable
-                users={users}
-                order={order}
-                orderBy={orderBy}
-                handleRequestSort={handleRequestSort}
-                page={page}
-                rowsPerPage={rowsPerPage}
-                handleChangePage={handleChangePage}
-                handleChangeRowsPerPage={handleChangeRowsPerPage}
-            />
-        </div>
+        <Box sx={theme.root}>
+            <Card sx={theme.standardCard}>
+                <Header>
+                    <Typography variant="h4" component="h1" gutterBottom sx={{fontWeight: 'bold'}}>
+                        New Signups
+                    </Typography>
+                </Header>
+                <NewSignupsTable
+                    users={users}
+                    order={order}
+                    orderBy={orderBy}
+                    handleRequestSort={handleRequestSort}
+                    page={page}
+                    rowsPerPage={rowsPerPage}
+                    handleChangePage={handleChangePage}
+                    handleChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+                <br/><br/>
+                <Header>
+                    <Typography variant="h4" component="h1" gutterBottom sx={{fontWeight: 'bold'}}>
+                        Open Teams
+                    </Typography>
+                </Header>
+
+                {loading ? (
+                    <LoadingSpinner/>
+                ) : error ? (
+                    <ErrorMessage message={error}/>
+                ) : (
+                    <TeamsTable
+                        teams={sortedTeams}
+                        order={order}
+                        orderBy={orderBy}
+                        handleRequestSort={handleTeamRequestSort}
+                        page={page}
+                        rowsPerPage={rowsPerPage}
+                        handleChangePage={handleTeamChangePage}
+                        handleChangeRowsPerPage={handleTeamChangeRowsPerPage}
+                        handleNullValue={handleNullValue}
+                        handleArrayValue={handleArrayValue}
+                    />
+                )}
+            </Card>
+        </Box>
     );
 };
 
